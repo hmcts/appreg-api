45d0593617d888efd587c37316a244d0845b6f75
WillJones <will.jones@HMCTS.NET>
2025-08-21 15:32:04 +0100

    ARCPOC-369 - Added temporary test endpoint to check login.

---
+++ src/main/java/uk/gov/hmcts/appregister/test/TestController.java
@@ -0,0 +1,16 @@
+package uk.gov.hmcts.appregister.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * TODO - REMOVE
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String loginSuccess() {
+        return "You successfully logged in!";
+    }
+}
