5d80ad8a2393223b297857900416979a24626e97
Michael_Estebanez <michael.estebanez@cgi.com>
2025-09-18 10:07:09 +0100

    Add open api spec generation

---
+++ src/main/java/uk/gov/hmcts/appregister/criminaljusticearea/controller/CriminalJusticeAreaController.java
@@ -0,0 +1,6 @@
+package uk.gov.hmcts.appregister.criminaljusticearea.controller;
+
+import uk.gov.hmcts.appregister.generated.api.CriminalJusticeAreasApi;
+
+public class CriminalJusticeAreaController implements CriminalJusticeAreasApi {
+}
