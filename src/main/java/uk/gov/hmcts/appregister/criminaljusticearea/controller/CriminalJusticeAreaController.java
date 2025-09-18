a1ed6bef6881a247cb76763452b7813a1f872cae
Michael_Estebanez <michael.estebanez@cgi.com>
2025-09-18 10:11:53 +0100

    Commit initial build using open api generated classes

--- src/main/java/uk/gov/hmcts/appregister/criminaljusticearea/controller/CriminalJusticeAreaController.java
+++
@@ -1,6 +0,0 @@
-package uk.gov.hmcts.appregister.criminaljusticearea.controller;
-
-import uk.gov.hmcts.appregister.generated.api.CriminalJusticeAreasApi;
-
-public class CriminalJusticeAreaController implements CriminalJusticeAreasApi {
-}
