18c9cd7d4350554152baf78044122d43a510de39
Michael Estebanez <mfesteba@yahoo.co.uk>
2025-12-09 18:18:11 +0000

    Disable action test

--- src/integrationTest/java/uk/gov/hmcts/appregister/controller/ApplicationEntryControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/appregister/controller/ApplicationEntryControllerTest.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 import java.util.function.UnaryOperator;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
@@ -27,6 +28,7 @@ import uk.gov.hmcts.appregister.testutils.controller.RestEndpointDescription;
 import uk.gov.hmcts.appregister.testutils.token.TokenGenerator;
 import uk.gov.hmcts.appregister.testutils.util.PagingAssertionUtil;
 
+@Disabled
 public class ApplicationEntryControllerTest extends AbstractSecurityControllerTest {
     private static final String WEB_CONTEXT = "application-list-entries";
 
