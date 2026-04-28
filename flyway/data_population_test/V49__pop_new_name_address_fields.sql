f18371a37bbec93315483ba90dfd24920debd1b4
colin.bradshaw <colin.bradshaw@cgi.com>
2026-04-28 16:59:15 +0100

    ARCPOC-1325 - Fix up test data.

--- flyway/data_population_test/V49__pop_new_name_address_fields.sql
+++ flyway/data_population_test/V49__pop_new_name_address_fields.sql
@@ -2,7 +2,22 @@
 
 -- Version Control
 -- V1.0  	Matthew Harman  28/04/2026	Initial Version
+-- V1.1.  Colin Bradshaw. 28/04/2026  Fix up test data
 --
+
+-- Normalise legacy test fixtures before updating rows protected by
+-- name_address_name_or_person_chk.
+update name_address
+set name = null
+where na_id in (1, 3);
+
+update name_address
+set title = null,
+    forename_1 = null,
+    forename_2 = null,
+    forename_3 = null,
+    surname = null
+where na_id = 2;
 
 update name_address
 set first_name = forename_1;
