a2c86285ddba8b5bb8ace94b9814ecc6329ac01c
colin.bradshaw <colin.bradshaw@cgi.com>
2026-06-01 18:37:26 +0100

    ARCPOC-1388: - Override "produces" for success path.

--- src/main/java/uk/gov/hmcts/appregister/admin/controller/AdminAPIController.java
+++ src/main/java/uk/gov/hmcts/appregister/admin/controller/AdminAPIController.java
@@ -2,10 +2,12 @@ package uk.gov.hmcts.appregister.admin.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PutMapping;
 import uk.gov.hmcts.appregister.admin.service.AdminAPIService;
 import uk.gov.hmcts.appregister.common.security.RoleNames;
 import uk.gov.hmcts.appregister.generated.api.AdminApi;
@@ -19,14 +21,19 @@ import uk.gov.hmcts.appregister.generated.model.JobStatus;
 @Controller
 @RequiredArgsConstructor
 public class AdminAPIController implements AdminApi {
+    private static final MediaType VND_JSON_V1 =
+            MediaType.parseMediaType("application/vnd.hmcts.appreg.v1+json");
 
     private final AdminAPIService adminAPIService;
 
     @Override
+    @PutMapping(
+            value = AdminApi.PATH_ENABLE_DISABLE_DATABASE_JOB_BY_NAME,
+            produces = {"application/vnd.hmcts.appreg.v1+json", "application/problem+json"})
     public ResponseEntity<Void> enableDisableDatabaseJobByName(
             AdminJobType jobName, Boolean enable) {
         adminAPIService.enableDisableDatabaseJobByName(jobName, enable);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().varyBy("Accept").contentType(VND_JSON_V1).build();
     }
 
     @Override
@@ -36,10 +43,13 @@ public class AdminAPIController implements AdminApi {
     }
 
     @Override
+    @PutMapping(
+            value = AdminApi.PATH_UPDATE_DATABASE_JOB_RETENTION_PERIOD_BY_NAME,
+            produces = {"application/vnd.hmcts.appreg.v1+json", "application/problem+json"})
     public ResponseEntity<Void> updateDatabaseJobRetentionPeriodByName(
             AdminJobType jobName, Integer retentionPeriodDays) {
         adminAPIService.updateDatabaseJobRetentionPeriodByName(jobName, retentionPeriodDays);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().varyBy("Accept").contentType(VND_JSON_V1).build();
     }
 
     @Override
