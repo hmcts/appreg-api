c99a8b3a72c9ac4c598eb3f4a6368188700c5259
Phil Head - CGI <152269503+philhead1@users.noreply.github.com>
2026-06-08 10:19:22 +0100

    WIP

---
+++ src/main/java/uk/gov/hmcts/appregister/config/CSDSMockConfig.java
@@ -0,0 +1,45 @@
+package uk.gov.hmcts.appregister.config;
+
+import com.github.tomakehurst.wiremock.common.ClasspathFileSource;
+import com.github.tomakehurst.wiremock.common.FileSource;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import com.github.tomakehurst.wiremock.WireMockServer;
+
+@Configuration
+@Slf4j
+@Profile("csds-mock")
+public class CSDSMockConfig {
+
+    @Value("${appreg.csds.wiremock.port:0}")
+    private int wireMockPort;
+
+    // This class is used to activate the csds-wiremock profile which will start a WireMock server with the CSDS stubs
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public WireMockServer csdsWireMockServer() {
+        log.warn("Starting WireMock server for CSDS stubs on port {}", wireMockPort);
+
+        WireMockConfiguration config = new WireMockConfiguration();
+        config.port(wireMockPort);
+        config.fileSource(fileSource("csds/stubs"));
+        return new WireMockServer(config);
+    }
+
+    private FileSource fileSource(String path) {
+        FileSource fs = null;
+        try {
+            fs = new ClasspathFileSource(path);
+        }
+        catch(Exception x) {
+            log.debug("Running via executable jar; using BOOT-INF folder.");
+            fs = new ClasspathFileSource("BOOT-INF/classes/" + path);
+        }
+        return fs;
+    }
+}
