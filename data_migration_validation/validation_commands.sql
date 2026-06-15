6f590019bebd2ac6f8593310e2a9b9422cf53ad4
mharman1973 <matthew.harman@hmcts.net>
2026-06-15 13:29:37 +0100

    ARCPOC-1432

--- data_migration_validation/validation_commands.sql
+++ data_migration_validation/validation_commands.sql
@@ -1,10 +1,10 @@
-"c:\Program Files\PostgreSQL\16\bin\psql.exe" --set=ON_ERROR_STOP=1 -c "\copy data_validation.oracle_column_metadata(owner, table_name, column_name, data_type, char_length, nullable, suggested_pg_type) FROM 'oracle_metadata.csv' CSV QUOTE ''''" "postgresql://hmcts:<pwd>@appreg-dev.postgres.database.azure.com:5432/appreg-db"
+"c:\Program Files\PostgreSQL\18\bin\psql.exe" --set=ON_ERROR_STOP=1 -c "\copy data_validation.oracle_column_metadata(owner, table_name, column_name, data_type, char_length, nullable, suggested_pg_type) FROM 'oracle_metadata.csv' CSV QUOTE ''''" "postgresql://hmcts:<pwd>@appreg-dev.postgres.database.azure.com:5432/appreg-db"
 
-"c:\Program Files\PostgreSQL\16\bin\psql.exe" --set=ON_ERROR_STOP=1 -c "\copy data_validation.oracle_rowcounts(owner, table_name, row_count) FROM 'oracle_rowcounts.csv' CSV QUOTE ''''" "postgresql://hmcts:<pwd>@appreg-dev.postgres.database.azure.com:5432/appreg-db"
+"c:\Program Files\PostgreSQL\18\bin\psql.exe" --set=ON_ERROR_STOP=1 -c "\copy data_validation.oracle_rowcounts(owner, table_name, row_count) FROM 'oracle_rowcounts.csv' CSV QUOTE ''''" "postgresql://hmcts:<pwd>@appreg-dev.postgres.database.azure.com:5432/appreg-db"
 
-"c:\Program Files\PostgreSQL\16\bin\psql.exe" --set=ON_ERROR_STOP=1 -c "\copy data_validation.oracle_counts_by_date(owner, table_name, bucket_label, row_count) FROM 'oracle_counts_by_date.csv' CSV QUOTE ''''" "postgresql://hmcts:<pwd>@appreg-dev.postgres.database.azure.com:5432/appreg-db"
+"c:\Program Files\PostgreSQL\18\bin\psql.exe" --set=ON_ERROR_STOP=1 -c "\copy data_validation.oracle_counts_by_date(owner, table_name, bucket_label, row_count) FROM 'oracle_counts_by_date.csv' CSV QUOTE ''''" "postgresql://hmcts:<pwd>@appreg-dev.postgres.database.azure.com:5432/appreg-db"
 
-"c:\Program Files\PostgreSQL\16\bin\psql.exe" --set=ON_ERROR_STOP=1 -c "\copy data_validation.oracle_column_analysis(owner, table_name, column_name, metric, metric_value) FROM 'oracle_column_analysis.csv' CSV QUOTE ''''" "postgresql://hmcts:<pwd>@appreg-dev.postgres.database.azure.com:5432/appreg-db"
+"c:\Program Files\PostgreSQL\18\bin\psql.exe" --set=ON_ERROR_STOP=1 -c "\copy data_validation.oracle_column_analysis(owner, table_name, column_name, metric, metric_value) FROM 'oracle_column_analysis.csv' CSV QUOTE ''''" "postgresql://hmcts:<pwd>@appreg-dev.postgres.database.azure.com:5432/appreg-db"
 
 -- populate the postgres side
 select data_validation.refresh_pg_counts();
