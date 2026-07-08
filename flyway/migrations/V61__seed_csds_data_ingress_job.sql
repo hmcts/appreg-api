1a0bb9c3dbe7b523506ba2d897abb054ad0f00c0
colin.bradshaw <colin.bradshaw@cgi.com>
2026-07-08 17:07:52 +0100

    ARCPOC-1430:
    - Update flyway numbering to accomodate incoming PR.

--- flyway/migrations/V61__seed_csds_data_ingress_job.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO database_jobs (dj_id, job_name, job_enabled, job_last_ran, job_metadata)
-VALUES (
-    COALESCE((SELECT MAX(dj_id) + 1 FROM database_jobs), 1),
-    'CSDS_DATA_INGRESS',
-    'Y',
-    NULL,
-    NULL
-)
-ON CONFLICT (job_name) DO NOTHING;
