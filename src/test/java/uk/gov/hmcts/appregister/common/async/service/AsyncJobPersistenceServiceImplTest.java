19584e9296e90d3d2361015945a69a5fcf71a87b
Phil Head - CGI <152269503+philhead1@users.noreply.github.com>
2026-07-20 17:38:27 +0100

    Added comment to new unit test

--- src/test/java/uk/gov/hmcts/appregister/common/async/service/AsyncJobPersistenceServiceImplTest.java
+++ src/test/java/uk/gov/hmcts/appregister/common/async/service/AsyncJobPersistenceServiceImplTest.java
@@ -260,6 +260,7 @@ class AsyncJobPersistenceServiceImplTest {
         var jobId = JobIdRequest.builder().id(UUID.randomUUID()).userName("tester").build();
         var asyncJob = AsyncJob.builder().uuid(jobId.getId()).userName("tester").build();
         when(asyncJobRepository.findByJobId(jobId.getId(), "tester")).thenReturn(asyncJob);
+        // Original maxLength was 4000, so we create a failure message that is longer than that to test truncation
         var failureMessage = "x".repeat(4000);
 
         service.setFailure(jobId, failureMessage);
