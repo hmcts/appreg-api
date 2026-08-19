hmcts/appreg-frontend

fix: use trusted identity for Codex PR publication

.github/scripts/test-codex-verify-publisher.py
@@ -1,0 +1,110 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+
+import importlib.util
+import io
+import json
+import sys
+import unittest
+from pathlib import Path
+from typing import Any
+
+
+MODULE_PATH = Path(__file__).with_name("codex-verify-publisher.py")
+SPEC = importlib.util.spec_from_file_location("codex_verify_publisher", MODULE_PATH)
+assert SPEC is not None and SPEC.loader is not None
+MODULE = importlib.util.module_from_spec(SPEC)
+sys.modules[SPEC.name] = MODULE
+SPEC.loader.exec_module(MODULE)
+
+
+class Response(io.BytesIO):
+    def __enter__(self) -> "Response":
+        return self
+
+    def __exit__(self, *_args: object) -> None:
+        self.close()
+
+
+class PublisherValidationTests(unittest.TestCase):
+    def payloads(self) -> tuple[dict[str, Any], dict[str, Any]]:
+        return (
+            {"login": "appreg-codex-bot", "type": "User"},
+            {
+                "full_name": "hmcts/appreg-frontend",
+                "permissions": {"pull": True, "push": True},
+            },
+        )
+
+    def test_accepts_expected_publisher_with_push_permission(self) -> None:
+        user, repository = self.payloads()
+        login = MODULE.validate_publisher(
+            "APPREG-CODEX-BOT", "hmcts/appreg-frontend", user, repository
+        )
+        self.assertEqual(login, "appreg-codex-bot")
+
+    def test_rejects_unexpected_publisher(self) -> None:
+        user, repository = self.payloads()
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "expected trusted-bot"):
+            MODULE.validate_publisher("trusted-bot", "hmcts/appreg-frontend", user, repository)
+
+    def test_rejects_default_actions_identity(self) -> None:
+        _, repository = self.payloads()
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "default GitHub Actions"):
+            MODULE.validate_publisher(
+                "github-actions[bot]",
+                "hmcts/appreg-frontend",
+                {"login": "github-actions[bot]", "type": "Bot"},
+                repository,
+            )
+
+    def test_rejects_non_user_identity(self) -> None:
+        _, repository = self.payloads()
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "dedicated machine user"):
+            MODULE.validate_publisher(
+                "appreg-codex-bot",
+                "hmcts/appreg-frontend",
+                {"login": "appreg-codex-bot", "type": "Bot"},
+                repository,
+            )
+
+    def test_rejects_token_without_push_permission(self) -> None:
+        user, repository = self.payloads()
+        repository["permissions"]["push"] = False
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "does not have push"):
+            MODULE.validate_publisher(
+                "appreg-codex-bot", "hmcts/appreg-frontend", user, repository
+            )
+
+    def test_rejects_unexpected_repository(self) -> None:
+        user, repository = self.payloads()
+        repository["full_name"] = "hmcts/another-repository"
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "unexpected repository"):
+            MODULE.validate_publisher(
+                "appreg-codex-bot", "hmcts/appreg-frontend", user, repository
+            )
+
+    def test_rejects_invalid_repository_name(self) -> None:
+        user, repository = self.payloads()
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "valid owner/repository"):
+            MODULE.validate_publisher("appreg-codex-bot", "../appreg-frontend", user, repository)
+
+    def test_client_uses_bearer_token_without_putting_it_in_url(self) -> None:
+        captured: dict[str, Any] = {}
+
+        def opener(request: Any, timeout: int) -> Response:
+            captured["url"] = request.full_url
+            captured["authorization"] = request.get_header("Authorization")
+            captured["timeout"] = timeout
+            return Response(json.dumps({"login": "appreg-codex-bot"}).encode())
+
+        client = MODULE.GitHubClient("https://api.github.test", "test-secret", opener=opener)
+        self.assertEqual(client.get_json("/user"), {"login": "appreg-codex-bot"})
+        self.assertEqual(captured["url"], "https://api.github.test/user")
+        self.assertEqual(captured["authorization"], "Bearer test-secret")
+        self.assertEqual(captured["timeout"], 20)
+
+
+if __name__ == "__main__":
+    unittest.main()

bin/codex-local-pipeline.sh
@@ -129,7 +129,7 @@ PYTHONPYCACHEPREFIX="${python_cache}" python3 .github/scripts/test-codex-jira-ve
 PYTHONPYCACHEPREFIX="${python_cache}" python3 .github/scripts/test-codex-pr-review-handoff.py
 PYTHONPYCACHEPREFIX="${python_cache}" python3 .github/scripts/test-validate-codex-plan.py
 PYTHONPYCACHEPREFIX="${python_cache}" python3 .github/scripts/test-codex-plan-handoff.py
-PYTHONPYCACHEPREFIX="${python_cache}" python3 .github/scripts/test-codex-approve-pr-workflows.py
+PYTHONPYCACHEPREFIX="${python_cache}" python3 .github/scripts/test-codex-verify-publisher.py
 
 log "Validating workflow YAML syntax"
 if command -v ruby >/dev/null 2>&1; then
@@ -642,6 +642,95 @@ revision_pinned_workflows.each do |workflow_name|
       errors << "#{path}:#{job_name} must check out the captured trusted SHA"
     end
   end
+end
+
+publisher_token = "${{ secrets.CODEX_GITHUB_TOKEN }}"
+publisher_login = "${{ vars.CODEX_PUBLISHER_LOGIN }}"
+publisher_specs = [
+  [".github/workflows/codex_jira_dispatch.yml", "publish-pr", "publish"],
+  [".github/workflows/codex_jira_dispatch.yml", "publish-published-pr-repair-1", "publish"],
+  [".github/workflows/codex_pr_review_feedback.yml", "codex-review-publish", "publish"],
+  [".github/workflows/codex_pr_review_feedback.yml", "codex-review-external-republish", "publish"],
+  [".github/workflows/codex_merge_conflict_resolution.yml", "publish-conflict-resolution", "publish"],
+  [".github/workflows/codex_runner_smoke.yml", "branch-smoke", "smoke"],
+]
+publisher_job_keys = publisher_specs.map { |path, job_name, _| [path, job_name] }
+
+publisher_specs.each do |path, job_name, publish_step_id|
+  workflow = YAML.load_file(path)
+  job = workflow.fetch("jobs", {}).fetch(job_name, {})
+  permissions = job.fetch("permissions", {}) || {}
+  unless permissions == { "contents" => "read" }
+    errors << "#{path}:#{job_name} must give the default GITHUB_TOKEN contents: read only"
+  end
+
+  job_env = job.fetch("env", {}) || {}
+  if job_env.key?("GH_TOKEN") || job_env.key?("CODEX_PUBLISHER_LOGIN")
+    errors << "#{path}:#{job_name} must not expose publisher credentials at job scope"
+  end
+
+  steps = job.fetch("steps", [])
+  verify_index = steps.index do |step|
+    step.is_a?(Hash) &&
+      step.fetch("run", "") == "python3 -I .github/scripts/codex-verify-publisher.py"
+  end
+  publish_index = steps.index do |step|
+    step.is_a?(Hash) && step.fetch("id", "") == publish_step_id
+  end
+
+  if verify_index.nil? || publish_index.nil? || verify_index >= publish_index
+    errors << "#{path}:#{job_name} must verify the trusted publisher before publishing"
+    next
+  end
+
+  [verify_index, publish_index].each do |index|
+    env = steps.fetch(index).fetch("env", {}) || {}
+    unless env.fetch("GH_TOKEN", "") == publisher_token &&
+           env.fetch("CODEX_PUBLISHER_LOGIN", "") == publisher_login
+      errors << "#{path}:#{job_name} must scope the trusted publisher secret and login to verifier/publisher steps"
+    end
+  end
+
+  steps.each_with_index do |step, index|
+    next unless step.is_a?(Hash)
+    next if [verify_index, publish_index].include?(index)
+    if step.inspect.include?("CODEX_GITHUB_TOKEN")
+      errors << "#{path}:#{job_name} exposes the publisher token outside verifier/publisher steps"
+    end
+  end
+end
+
+Dir[".github/workflows/*.yml", ".github/workflows/*.yaml"].each do |path|
+  workflow = YAML.load_file(path)
+  workflow.fetch("jobs", {}).each do |job_name, job|
+    if job.inspect.include?("CODEX_GITHUB_TOKEN") &&
+       !publisher_job_keys.include?([path, job_name])
+      errors << "#{path}:#{job_name} must not receive the trusted publisher token"
+    end
+  end
+end
+
+if File.exist?(".github/workflows/codex_approve_pr_workflows.yml") ||
+   Dir[".github/workflows/*"].any? { |path| File.read(path).include?("codex_approve_pr_workflows") }
+  errors << "unsupported pull-request workflow auto-approval machinery must not be present"
+end
+checks_workflow = YAML.load_file(".github/workflows/checks.yml")
+unless checks_workflow.fetch("permissions", {}) == { "contents" => "read" }
+  errors << ".github/workflows/checks.yml must use contents: read permissions"
+end
+checks_checkout = checks_workflow.fetch("jobs", {}).fetch("build", {}).fetch("steps", []).find do |step|
+  step.is_a?(Hash) && step.fetch("uses", "").start_with?("actions/checkout@")
+end
+unless checks_checkout && checks_checkout.fetch("with", {}).fetch("persist-credentials", true) == false
+  errors << ".github/workflows/checks.yml must disable persisted checkout credentials"
+end
+
+codeql_workflow = YAML.load_file(".github/workflows/codeql.yaml")
+codeql_checkout = codeql_workflow.fetch("jobs", {}).fetch("analyze", {}).fetch("steps", []).find do |step|
+  step.is_a?(Hash) && step.fetch("uses", "").start_with?("actions/checkout@")
+end
+unless codeql_checkout && codeql_checkout.fetch("with", {}).fetch("persist-credentials", true) == false
+  errors << ".github/workflows/codeql.yaml must disable persisted checkout credentials"
 end
 
 abort(errors.join("\n")) unless errors.empty?

hmcts/appreg-api

.github/scripts/test-codex-verify-publisher.py
@@ -1,0 +1,110 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+
+import importlib.util
+import io
+import json
+import sys
+import unittest
+from pathlib import Path
+from typing import Any
+
+
+MODULE_PATH = Path(__file__).with_name("codex-verify-publisher.py")
+SPEC = importlib.util.spec_from_file_location("codex_verify_publisher", MODULE_PATH)
+assert SPEC is not None and SPEC.loader is not None
+MODULE = importlib.util.module_from_spec(SPEC)
+sys.modules[SPEC.name] = MODULE
+SPEC.loader.exec_module(MODULE)
+
+
+class Response(io.BytesIO):
+    def __enter__(self) -> "Response":
+        return self
+
+    def __exit__(self, *_args: object) -> None:
+        self.close()
+
+
+class PublisherValidationTests(unittest.TestCase):
+    def payloads(self) -> tuple[dict[str, Any], dict[str, Any]]:
+        return (
+            {"login": "appreg-codex-bot", "type": "User"},
+            {
+                "full_name": "hmcts/appreg-api",
+                "permissions": {"pull": True, "push": True},
+            },
+        )
+
+    def test_accepts_expected_publisher_with_push_permission(self) -> None:
+        user, repository = self.payloads()
+        login = MODULE.validate_publisher(
+            "APPREG-CODEX-BOT", "hmcts/appreg-api", user, repository
+        )
+        self.assertEqual(login, "appreg-codex-bot")
+
+    def test_rejects_unexpected_publisher(self) -> None:
+        user, repository = self.payloads()
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "expected trusted-bot"):
+            MODULE.validate_publisher("trusted-bot", "hmcts/appreg-api", user, repository)
+
+    def test_rejects_default_actions_identity(self) -> None:
+        _, repository = self.payloads()
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "default GitHub Actions"):
+            MODULE.validate_publisher(
+                "github-actions[bot]",
+                "hmcts/appreg-api",
+                {"login": "github-actions[bot]", "type": "Bot"},
+                repository,
+            )
+
+    def test_rejects_non_user_identity(self) -> None:
+        _, repository = self.payloads()
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "dedicated machine user"):
+            MODULE.validate_publisher(
+                "appreg-codex-bot",
+                "hmcts/appreg-api",
+                {"login": "appreg-codex-bot", "type": "Bot"},
+                repository,
+            )
+
+    def test_rejects_token_without_push_permission(self) -> None:
+        user, repository = self.payloads()
+        repository["permissions"]["push"] = False
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "does not have push"):
+            MODULE.validate_publisher(
+                "appreg-codex-bot", "hmcts/appreg-api", user, repository
+            )
+
+    def test_rejects_unexpected_repository(self) -> None:
+        user, repository = self.payloads()
+        repository["full_name"] = "hmcts/another-repository"
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "unexpected repository"):
+            MODULE.validate_publisher(
+                "appreg-codex-bot", "hmcts/appreg-api", user, repository
+            )
+
+    def test_rejects_invalid_repository_name(self) -> None:
+        user, repository = self.payloads()
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "valid owner/repository"):
+            MODULE.validate_publisher("appreg-codex-bot", "../appreg-api", user, repository)
+
+    def test_client_uses_bearer_token_without_putting_it_in_url(self) -> None:
+        captured: dict[str, Any] = {}
+
+        def opener(request: Any, timeout: int) -> Response:
+            captured["url"] = request.full_url
+            captured["authorization"] = request.get_header("Authorization")
+            captured["timeout"] = timeout
+            return Response(json.dumps({"login": "appreg-codex-bot"}).encode())
+
+        client = MODULE.GitHubClient("https://api.github.test", "test-secret", opener=opener)
+        self.assertEqual(client.get_json("/user"), {"login": "appreg-codex-bot"})
+        self.assertEqual(captured["url"], "https://api.github.test/user")
+        self.assertEqual(captured["authorization"], "Bearer test-secret")
+        self.assertEqual(captured["timeout"], 20)
+
+
+if __name__ == "__main__":
+    unittest.main()

bin/codex-local-pipeline.sh
@@ -137,7 +137,7 @@ PYTHONPYCACHEPREFIX="${python_cache}" python3 .github/scripts/test-codex-publish
 PYTHONPYCACHEPREFIX="${python_cache}" python3 .github/scripts/test-codex-pr-review-handoff.py
 PYTHONPYCACHEPREFIX="${python_cache}" python3 .github/scripts/test-validate-codex-plan.py
 PYTHONPYCACHEPREFIX="${python_cache}" python3 .github/scripts/test-codex-plan-handoff.py
-PYTHONPYCACHEPREFIX="${python_cache}" python3 .github/scripts/test-codex-approve-pr-workflows.py
+PYTHONPYCACHEPREFIX="${python_cache}" python3 .github/scripts/test-codex-verify-publisher.py
 
 log "Validating workflow YAML syntax"
 if command -v ruby >/dev/null 2>&1; then
@@ -592,6 +592,97 @@ revision_pinned_workflows.each do |workflow_name|
       end
     elsif moving_checkouts.any?
       errors << "#{path}:#{job_name} must check out the captured trusted SHA"
+    end
+  end
+end
+
+publisher_token = "${{ secrets.CODEX_GITHUB_TOKEN }}"
+publisher_login = "${{ vars.CODEX_PUBLISHER_LOGIN }}"
+publisher_specs = [
+  [".github/workflows/codex_jira_dispatch.yml", "publish-pr", "publish"],
+  [".github/workflows/codex_jira_dispatch.yml", "publish-published-pr-repair-1", "publish"],
+  [".github/workflows/codex_pr_review_feedback.yml", "codex-review-publish", "publish"],
+  [".github/workflows/codex_pr_review_feedback.yml", "codex-review-external-republish", "publish"],
+  [".github/workflows/codex_merge_conflict_resolution.yml", "publish-conflict-resolution", "publish"],
+  [".github/workflows/codex_runner_smoke.yml", "branch-smoke", "smoke"],
+]
+publisher_job_keys = publisher_specs.map { |path, job_name, _| [path, job_name] }
+
+publisher_specs.each do |path, job_name, publish_step_id|
+  workflow = YAML.load_file(path)
+  job = workflow.fetch("jobs", {}).fetch(job_name, {})
+  permissions = job.fetch("permissions", {}) || {}
+  unless permissions == { "contents" => "read" }
+    errors << "#{path}:#{job_name} must give the default GITHUB_TOKEN contents: read only"
+  end
+
+  job_env = job.fetch("env", {}) || {}
+  if job_env.key?("GH_TOKEN") || job_env.key?("CODEX_PUBLISHER_LOGIN")
+    errors << "#{path}:#{job_name} must not expose publisher credentials at job scope"
+  end
+
+  steps = job.fetch("steps", [])
+  verify_index = steps.index do |step|
+    step.is_a?(Hash) &&
+      step.fetch("run", "") == "python3 -I .github/scripts/codex-verify-publisher.py"
+  end
+  publish_index = steps.index do |step|
+    step.is_a?(Hash) && step.fetch("id", "") == publish_step_id
+  end
+
+  if verify_index.nil? || publish_index.nil? || verify_index >= publish_index
+    errors << "#{path}:#{job_name} must verify the trusted publisher before publishing"
+    next
+  end
+
+  [verify_index, publish_index].each do |index|
+    env = steps.fetch(index).fetch("env", {}) || {}
+    unless env.fetch("GH_TOKEN", "") == publisher_token &&
+           env.fetch("CODEX_PUBLISHER_LOGIN", "") == publisher_login
+      errors << "#{path}:#{job_name} must scope the trusted publisher secret and login to verifier/publisher steps"
+    end
+  end
+
+  steps.each_with_index do |step, index|
+    next unless step.is_a?(Hash)
+    next if [verify_index, publish_index].include?(index)
+    if step.inspect.include?("CODEX_GITHUB_TOKEN")
+      errors << "#{path}:#{job_name} exposes the publisher token outside verifier/publisher steps"
+    end
+  end
+end
+
+Dir[".github/workflows/*.yml", ".github/workflows/*.yaml"].each do |path|
+  workflow = YAML.load_file(path)
+  workflow.fetch("jobs", {}).each do |job_name, job|
+    if job.inspect.include?("CODEX_GITHUB_TOKEN") &&
+       !publisher_job_keys.include?([path, job_name])
+      errors << "#{path}:#{job_name} must not receive the trusted publisher token"
+    end
+  end
+end
+
+if File.exist?(".github/workflows/codex_approve_pr_workflows.yml") ||
+   Dir[".github/workflows/*"].any? { |path| File.read(path).include?("codex_approve_pr_workflows") }
+  errors << "unsupported pull-request workflow auto-approval machinery must not be present"
+end
+[
+  ".github/workflows/existing_flyway_change_prevention.yml",
+  ".github/workflows/flyway-dupe-checker.yml",
+].each do |path|
+  workflow = YAML.load_file(path)
+  unless workflow.fetch("permissions", {}) == { "contents" => "read" }
+    errors << "#{path} must use contents: read permissions"
+  end
+  if File.read(path).include?("CODEX_GITHUB_TOKEN")
+    errors << "#{path} must not receive the trusted publisher token"
+  end
+  workflow.fetch("jobs", {}).each do |job_name, job|
+    checkouts = job.fetch("steps", []).select do |step|
+      step.is_a?(Hash) && step.fetch("uses", "").start_with?("actions/checkout@")
+    end
+    if checkouts.empty? || checkouts.any? { |step| step.fetch("with", {}).fetch("persist-credentials", true) != false }
+      errors << "#{path}:#{job_name} must use checkout without persisted credentials"
     end
   end
 end