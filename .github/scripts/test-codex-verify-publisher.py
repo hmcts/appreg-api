hmcts/appreg-frontend

Publish Codex changes through GitHub App

.github/scripts/test-codex-verify-publisher.py
@@ -28,67 +28,83 @@ class Response(io.BytesIO):
 
 
 class PublisherValidationTests(unittest.TestCase):
-    def payloads(self) -> tuple[dict[str, Any], dict[str, Any]]:
+    def payloads(self) -> tuple[dict[str, Any], dict[str, Any], dict[str, Any]]:
         return (
-            {"login": "appreg-codex-bot", "type": "User"},
+            {
+                "id": 12345,
+                "app_slug": "hmcts-codex-agent",
+                "account": {"login": "hmcts"},
+                "permissions": {
+                    "contents": "write",
+                    "pull_requests": "write",
+                    "issues": "write",
+                    "workflows": "write",
+                },
+            },
             {
                 "full_name": "hmcts/appreg-frontend",
                 "permissions": {"pull": True, "push": True},
             },
+            {"login": "hmcts-codex-agent[bot]", "id": 98765, "type": "Bot"},
         )
 
-    def test_accepts_expected_publisher_with_push_permission(self) -> None:
-        user, repository = self.payloads()
-        login = MODULE.validate_publisher(
-            "APPREG-CODEX-BOT", "hmcts/appreg-frontend", user, repository
+    def validate(self, **overrides: Any) -> tuple[str, str]:
+        installation, repository, bot = self.payloads()
+        return MODULE.validate_publisher(
+            overrides.get("app_slug", "hmcts-codex-agent"),
+            overrides.get("installation_id", "12345"),
+            overrides.get("repository_name", "hmcts/appreg-frontend"),
+            overrides.get("installation", installation),
+            overrides.get("repository", repository),
+            overrides.get("bot", bot),
         )
-        self.assertEqual(login, "appreg-codex-bot")
 
-    def test_rejects_unexpected_publisher(self) -> None:
-        user, repository = self.payloads()
-        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "expected trusted-bot"):
-            MODULE.validate_publisher("trusted-bot", "hmcts/appreg-frontend", user, repository)
+    def test_accepts_expected_app_installation_with_push_permission(self) -> None:
+        login, email = self.validate()
+        self.assertEqual(login, "hmcts-codex-agent[bot]")
+        self.assertEqual(email, "98765+hmcts-codex-agent[bot]@users.noreply.github.com")
 
-    def test_rejects_default_actions_identity(self) -> None:
-        _, repository = self.payloads()
-        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "default GitHub Actions"):
-            MODULE.validate_publisher(
-                "github-actions[bot]",
-                "hmcts/appreg-frontend",
-                {"login": "github-actions[bot]", "type": "Bot"},
-                repository,
-            )
+    def test_rejects_unexpected_app(self) -> None:
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "expected another-app"):
+            self.validate(app_slug="another-app")
 
-    def test_rejects_non_user_identity(self) -> None:
-        _, repository = self.payloads()
-        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "dedicated machine user"):
-            MODULE.validate_publisher(
-                "appreg-codex-bot",
-                "hmcts/appreg-frontend",
-                {"login": "appreg-codex-bot", "type": "Bot"},
-                repository,
-            )
+    def test_rejects_unexpected_installation(self) -> None:
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "unexpected App installation"):
+            self.validate(installation_id="54321")
+
+    def test_rejects_installation_owned_by_another_account(self) -> None:
+        installation, _, _ = self.payloads()
+        installation["account"]["login"] = "another-org"
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "not owned by hmcts"):
+            self.validate(installation=installation)
+
+    def test_rejects_missing_installation_permission(self) -> None:
+        installation, _, _ = self.payloads()
+        installation["permissions"]["issues"] = "read"
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "lacks required"):
+            self.validate(installation=installation)
 
     def test_rejects_token_without_push_permission(self) -> None:
-        user, repository = self.payloads()
+        _, repository, _ = self.payloads()
         repository["permissions"]["push"] = False
         with self.assertRaisesRegex(MODULE.PublisherVerificationError, "does not have push"):
-            MODULE.validate_publisher(
-                "appreg-codex-bot", "hmcts/appreg-frontend", user, repository
-            )
+            self.validate(repository=repository)
 
     def test_rejects_unexpected_repository(self) -> None:
-        user, repository = self.payloads()
+        _, repository, _ = self.payloads()
         repository["full_name"] = "hmcts/another-repository"
         with self.assertRaisesRegex(MODULE.PublisherVerificationError, "unexpected repository"):
-            MODULE.validate_publisher(
-                "appreg-codex-bot", "hmcts/appreg-frontend", user, repository
-            )
+            self.validate(repository=repository)
 
     def test_rejects_invalid_repository_name(self) -> None:
-        user, repository = self.payloads()
         with self.assertRaisesRegex(MODULE.PublisherVerificationError, "valid owner/repository"):
-            MODULE.validate_publisher("appreg-codex-bot", "../appreg-frontend", user, repository)
+            self.validate(repository_name="../appreg-frontend")
+
+    def test_rejects_non_bot_identity(self) -> None:
+        _, _, bot = self.payloads()
+        bot["type"] = "User"
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "bot identity"):
+            self.validate(bot=bot)
 
     def test_client_uses_bearer_token_without_putting_it_in_url(self) -> None:
         captured: dict[str, Any] = {}
@@ -97,11 +113,11 @@ class PublisherValidationTests(unittest.TestCase):
             captured["url"] = request.full_url
             captured["authorization"] = request.get_header("Authorization")
             captured["timeout"] = timeout
-            return Response(json.dumps({"login": "appreg-codex-bot"}).encode())
+            return Response(json.dumps({"id": 12345}).encode())
 
         client = MODULE.GitHubClient("https://api.github.test", "test-secret", opener=opener)
-        self.assertEqual(client.get_json("/user"), {"login": "appreg-codex-bot"})
-        self.assertEqual(captured["url"], "https://api.github.test/user")
+        self.assertEqual(client.get_json("/installation"), {"id": 12345})
+        self.assertEqual(captured["url"], "https://api.github.test/installation")
         self.assertEqual(captured["authorization"], "Bearer test-secret")
         self.assertEqual(captured["timeout"], 20)
 

hmcts/appreg-api

.github/scripts/test-codex-verify-publisher.py
@@ -28,67 +28,83 @@ class Response(io.BytesIO):
 
 
 class PublisherValidationTests(unittest.TestCase):
-    def payloads(self) -> tuple[dict[str, Any], dict[str, Any]]:
+    def payloads(self) -> tuple[dict[str, Any], dict[str, Any], dict[str, Any]]:
         return (
-            {"login": "appreg-codex-bot", "type": "User"},
+            {
+                "id": 12345,
+                "app_slug": "hmcts-codex-agent",
+                "account": {"login": "hmcts"},
+                "permissions": {
+                    "contents": "write",
+                    "pull_requests": "write",
+                    "issues": "write",
+                    "workflows": "write",
+                },
+            },
             {
                 "full_name": "hmcts/appreg-api",
                 "permissions": {"pull": True, "push": True},
             },
+            {"login": "hmcts-codex-agent[bot]", "id": 98765, "type": "Bot"},
         )
 
-    def test_accepts_expected_publisher_with_push_permission(self) -> None:
-        user, repository = self.payloads()
-        login = MODULE.validate_publisher(
-            "APPREG-CODEX-BOT", "hmcts/appreg-api", user, repository
+    def validate(self, **overrides: Any) -> tuple[str, str]:
+        installation, repository, bot = self.payloads()
+        return MODULE.validate_publisher(
+            overrides.get("app_slug", "hmcts-codex-agent"),
+            overrides.get("installation_id", "12345"),
+            overrides.get("repository_name", "hmcts/appreg-api"),
+            overrides.get("installation", installation),
+            overrides.get("repository", repository),
+            overrides.get("bot", bot),
         )
-        self.assertEqual(login, "appreg-codex-bot")
 
-    def test_rejects_unexpected_publisher(self) -> None:
-        user, repository = self.payloads()
-        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "expected trusted-bot"):
-            MODULE.validate_publisher("trusted-bot", "hmcts/appreg-api", user, repository)
+    def test_accepts_expected_app_installation_with_push_permission(self) -> None:
+        login, email = self.validate()
+        self.assertEqual(login, "hmcts-codex-agent[bot]")
+        self.assertEqual(email, "98765+hmcts-codex-agent[bot]@users.noreply.github.com")
 
-    def test_rejects_default_actions_identity(self) -> None:
-        _, repository = self.payloads()
-        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "default GitHub Actions"):
-            MODULE.validate_publisher(
-                "github-actions[bot]",
-                "hmcts/appreg-api",
-                {"login": "github-actions[bot]", "type": "Bot"},
-                repository,
-            )
+    def test_rejects_unexpected_app(self) -> None:
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "expected another-app"):
+            self.validate(app_slug="another-app")
 
-    def test_rejects_non_user_identity(self) -> None:
-        _, repository = self.payloads()
-        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "dedicated machine user"):
-            MODULE.validate_publisher(
-                "appreg-codex-bot",
-                "hmcts/appreg-api",
-                {"login": "appreg-codex-bot", "type": "Bot"},
-                repository,
-            )
+    def test_rejects_unexpected_installation(self) -> None:
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "unexpected App installation"):
+            self.validate(installation_id="54321")
+
+    def test_rejects_installation_owned_by_another_account(self) -> None:
+        installation, _, _ = self.payloads()
+        installation["account"]["login"] = "another-org"
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "not owned by hmcts"):
+            self.validate(installation=installation)
+
+    def test_rejects_missing_installation_permission(self) -> None:
+        installation, _, _ = self.payloads()
+        installation["permissions"]["issues"] = "read"
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "lacks required"):
+            self.validate(installation=installation)
 
     def test_rejects_token_without_push_permission(self) -> None:
-        user, repository = self.payloads()
+        _, repository, _ = self.payloads()
         repository["permissions"]["push"] = False
         with self.assertRaisesRegex(MODULE.PublisherVerificationError, "does not have push"):
-            MODULE.validate_publisher(
-                "appreg-codex-bot", "hmcts/appreg-api", user, repository
-            )
+            self.validate(repository=repository)
 
     def test_rejects_unexpected_repository(self) -> None:
-        user, repository = self.payloads()
+        _, repository, _ = self.payloads()
         repository["full_name"] = "hmcts/another-repository"
         with self.assertRaisesRegex(MODULE.PublisherVerificationError, "unexpected repository"):
-            MODULE.validate_publisher(
-                "appreg-codex-bot", "hmcts/appreg-api", user, repository
-            )
+            self.validate(repository=repository)
 
     def test_rejects_invalid_repository_name(self) -> None:
-        user, repository = self.payloads()
         with self.assertRaisesRegex(MODULE.PublisherVerificationError, "valid owner/repository"):
-            MODULE.validate_publisher("appreg-codex-bot", "../appreg-api", user, repository)
+            self.validate(repository_name="../appreg-api")
+
+    def test_rejects_non_bot_identity(self) -> None:
+        _, _, bot = self.payloads()
+        bot["type"] = "User"
+        with self.assertRaisesRegex(MODULE.PublisherVerificationError, "bot identity"):
+            self.validate(bot=bot)
 
     def test_client_uses_bearer_token_without_putting_it_in_url(self) -> None:
         captured: dict[str, Any] = {}
@@ -97,11 +113,11 @@ class PublisherValidationTests(unittest.TestCase):
             captured["url"] = request.full_url
             captured["authorization"] = request.get_header("Authorization")
             captured["timeout"] = timeout
-            return Response(json.dumps({"login": "appreg-codex-bot"}).encode())
+            return Response(json.dumps({"id": 12345}).encode())
 
         client = MODULE.GitHubClient("https://api.github.test", "test-secret", opener=opener)
-        self.assertEqual(client.get_json("/user"), {"login": "appreg-codex-bot"})
-        self.assertEqual(captured["url"], "https://api.github.test/user")
+        self.assertEqual(client.get_json("/installation"), {"id": 12345})
+        self.assertEqual(captured["url"], "https://api.github.test/installation")
         self.assertEqual(captured["authorization"], "Bearer test-secret")
         self.assertEqual(captured["timeout"], 20)
 